e2c34d81eb2655ac2476508e9f9e8ae7f19c8f4b
HiTechCharles <hitechcharles@gmail.com>
2024-11-05 19:49:04 -0500

    Fully functional
    lists videos in a folder, then allows you to select one.  selected folder is in code

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Workout_Videos_for_Tiffany
+{
+    partial class TWV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileListLB = new System.Windows.Forms.ListBox();
+            this.WorkoutLBL = new System.Windows.Forms.Label();
+            this.OKBTN = new System.Windows.Forms.Button();
+            this.RandomBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FileListLB
+            // 
+            this.FileListLB.FormattingEnabled = true;
+            this.FileListLB.ItemHeight = 39;
+            this.FileListLB.Location = new System.Drawing.Point(12, 58);
+            this.FileListLB.Name = "FileListLB";
+            this.FileListLB.Size = new System.Drawing.Size(556, 433);
+            this.FileListLB.Sorted = true;
+            this.FileListLB.TabIndex = 1;
+            // 
+            // WorkoutLBL
+            // 
+            this.WorkoutLBL.AutoSize = true;
+            this.WorkoutLBL.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.WorkoutLBL.Location = new System.Drawing.Point(12, 14);
+            this.WorkoutLBL.Name = "WorkoutLBL";
+            this.WorkoutLBL.Size = new System.Drawing.Size(556, 39);
+            this.WorkoutLBL.TabIndex = 0;
+            this.WorkoutLBL.Text = "Select a workout, then hit ENTER.";
+            // 
+            // OKBTN
+            // 
+            this.OKBTN.AccessibleName = "OK";
+            this.OKBTN.AutoSize = true;
+            this.OKBTN.Location = new System.Drawing.Point(19, 515);
+            this.OKBTN.Name = "OKBTN";
+            this.OKBTN.Size = new System.Drawing.Size(254, 51);
+            this.OKBTN.TabIndex = 2;
+            this.OKBTN.Text = "&OK";
+            this.OKBTN.UseVisualStyleBackColor = true;
+            this.OKBTN.UseWaitCursor = true;
+            this.OKBTN.Click += new System.EventHandler(this.OKBTN_Click);
+            // 
+            // RandomBTN
+            // 
+            this.RandomBTN.AccessibleName = "Random";
+            this.RandomBTN.AutoSize = true;
+            this.RandomBTN.Location = new System.Drawing.Point(314, 515);
+            this.RandomBTN.Name = "RandomBTN";
+            this.RandomBTN.Size = new System.Drawing.Size(254, 51);
+            this.RandomBTN.TabIndex = 3;
+            this.RandomBTN.Text = "&Random";
+            this.RandomBTN.UseVisualStyleBackColor = true;
+            this.RandomBTN.UseWaitCursor = true;
+            this.RandomBTN.Click += new System.EventHandler(this.RandomBTN_Click);
+            // 
+            // TWV
+            // 
+            this.AcceptButton = this.OKBTN;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 39F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(581, 588);
+            this.Controls.Add(this.RandomBTN);
+            this.Controls.Add(this.OKBTN);
+            this.Controls.Add(this.WorkoutLBL);
+            this.Controls.Add(this.FileListLB);
+            this.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "TWV";
+            this.Text = "Workout Videos for Tiffany";
+            this.Load += new System.EventHandler(this.TWV_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox FileListLB;
+        private System.Windows.Forms.Label WorkoutLBL;
+        public System.Windows.Forms.Button OKBTN;
+        public System.Windows.Forms.Button RandomBTN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Workout_Videos_for_Tiffany
+{
+    public partial class TWV : Form
+    {
+        public TWV()
+        {
+            InitializeComponent();
+        }
+
+        private void TWV_Load(object sender, EventArgs e)
+        {
+            String BaseName;  //holds file name without extension
+            string rootdir = @"M:\video\workout";  //directory containing files to look for
+            //get string array of files in RootDir *.iso files only
+            string[] FileList = Directory.GetFileSystemEntries(rootdir, "*.iso", SearchOption.TopDirectoryOnly);
+
+            //for each file name in FileList,
+            for (int i = 0; i < FileList.Length; i++)
+            {
+                string FilePath = FileList[i];  //store path
+                //chop off file extension for list
+                BaseName = Path.GetFileNameWithoutExtension(FilePath);
+                //Add Basename to Listbox
+                FileListLB.Items.Add(BaseName);
+            }
+        }
+
+        private void OKBTN_Click(object sender, EventArgs e)
+        {
+            //where to look for files
+            string rootdir = @"M:\video\workout";
+            //string array of files in RootDir, .Iso only
+            string[] FileList = Directory.GetFileSystemEntries(rootdir, "*.iso", SearchOption.TopDirectoryOnly);
+            //get path for listbox selected index
+            String VideoPath = FileList[FileListLB.SelectedIndex];
+
+            //start VLC video player full screen
+            //MUST put quotes around file name in arguments
+
+            using (System.Diagnostics.Process pProcess = new System.Diagnostics.Process())
+            {
+                pProcess.StartInfo.FileName = @"C:\Program Files\VideoLAN\VLC\vlc.exe";  //filename
+                pProcess.StartInfo.Arguments = "--aspect-ratio 16:9 -L -f \"" + VideoPath + "\""; //argument
+                pProcess.StartInfo.UseShellExecute = false;
+                pProcess.StartInfo.RedirectStandardOutput = true;
+                pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Maximized;
+                pProcess.StartInfo.CreateNoWindow = true; //not display a window
+                pProcess.Start();
+                string output = pProcess.StandardOutput.ReadToEnd(); //The output result
+                this.Close();  //close program after video starts
+            }
+        }
+
+        private void RandomBTN_Click(object sender, EventArgs e)
+        {
+            //pick random number between 0 and Listbox items
+            Random rnd = new Random();
+            FileListLB.SelectedIndex = rnd.Next(0, FileListLB.Items.Count);
+        }
+    }
+}
